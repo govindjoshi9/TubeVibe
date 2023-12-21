554c6b743266f7679a33a0fc54f6624b7d6dce47
govind <joshigovind740@gmail.com>
2023-12-22 01:50:44 +0530

    key

--- src/utils/fatchFromApi.js
+++ src/utils/fatchFromApi.js
@@ -7,11 +7,10 @@ const options = {
     maxResults: "50",
   },
   headers: {
-    "X-RapidAPI-Key": "aabf6a856dmsh972c58e1f638bdap1a6af4jsn7b9ded544c75",
+    "X-RapidAPI-Key": "process.env.REACT_APP_RAPID_API_KEY",
     "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
   },
 };
-
 export const fetchFromApi = async (url) => {
     const { data } = await axios.get(`${BASE_URL}/${url}`, options);
     return data;
